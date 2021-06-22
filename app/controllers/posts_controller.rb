490e22fcd8cdf21c0111c1159e4aada044e41d8a
zhongjinye <610505041@qq.com>
2021-06-22 11:00:40 +0800

    git push

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,30 @@ class PostsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def edit
+     @group = Group.find(params[:group_id])
+     @post = @group.posts.find(params[:id])
+   end
+
+   def update
+     @group = Group.find(params[:group_id])
+     @post = @group.posts.find(params[:id])
+
+     if @post.update(post_params)
+       redirect_to account_posts_path, notice: "Update Success"
+     else
+       render :edit
+     end
+   end
+
+   def destroy
+     @group = Group.find(params[:group_id])
+     @post = @group.posts.find(params[:id])
+     @post.destroy
+     flash[:alert] = "Post deleted"
+     redirect_to account_posts_path
+   end
+
   private
 
   def post_params
